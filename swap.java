cde659a666682df02258b16f7227a5c419938e5d
Ravi Yadav <raviyadav3929@gmail.com>
2025-09-05 14:03:42 +0530

    add to swapcode

---
+++ swap.java
@@ -0,0 +1,17 @@
+public class swap {
+    public static void main(String[] args) {
+        
+        int a =5;
+        int b =6;
+        System.out.println("before swaping a ="+a);
+        System.out.println("before swaping b ="+b);
+         
+        int temp =a;
+        a=b;
+        b= temp;
+        
+System.out.println("after swaping a ="+a);
+System.out.println("after swaping b =" +b);
+
+    }
+}
